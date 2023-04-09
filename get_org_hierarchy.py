54589d26499aa3a37a311d3da64141380dc750ed
jewel-t <130291847+jewel-t@users.noreply.github.com>
2023-04-09 21:54:24 +0800

    Update get_org_hierarchy.py

--- get_org_hierarchy.py
+++ get_org_hierarchy.py
@@ -3,15 +3,20 @@ import win32com.client
 outlook = win32com.client.Dispatch("Outlook.Application")
 namespace = outlook.GetNamespace("MAPI")
 
-email_address = "jsmith@example.com"  # Replace with the email address
-recipient = namespace.CreateRecipient(email_address)
-if recipient.Resolve():
-    exchange_user = recipient.AddressEntry.GetExchangeUser()
-    if exchange_user:
-        print(f"Alias: {exchange_user.Alias}")
-        print(f"Name: {exchange_user.Name}")
-        print(f"Department: {exchange_user.Department}")
+def get_subordinates(email_address):
+    recipient = namespace.CreateRecipient(email_address)
+    if recipient.Resolve():
+        exchange_user = recipient.AddressEntry.GetExchangeUser()
+        if exchange_user:
+            print(f"Alias: {exchange_user.Alias}")
+            print(f"Name: {exchange_user.Name}")
+            print(f"Department: {exchange_user.Department}")
+            for subordinate in exchange_user.GetDirectReports():
+                get_subordinates(subordinate.PrimarySmtpAddress)
+        else:
+            print(f"No ExchangeUser found for {email_address}")
     else:
-        print(f"No ExchangeUser found for {email_address}")
-else:
-    print(f"Unable to resolve {email_address}")
+        print(f"Unable to resolve {email_address}")
+
+# Replace with the email address of the employee whose subordinates you want to retrieve
+get_subordinates("jsmith@example.com")
