26fe55fb9cbc17db762755670459adb421f77cdc
jewel-t <130291847+jewel-t@users.noreply.github.com>
2023-04-09 20:44:16 +0800

    Create get_org_hierarchy.py

---
+++ get_org_hierarchy.py
@@ -0,0 +1,35 @@
+import win32com.client
+
+# Connect to Outlook application
+outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
+
+# Get the Global Address List
+gal = outlook.Session.GetGlobalAddressList()
+
+# Define a recursive function to traverse the organization hierarchy
+def traverse_org_hierarchy(employee_code, direct_reports):
+    # Get the contact information for the employee
+    employee = gal.AddressEntries.GetFirst()
+    while employee:
+        if employee.Type == "EX":
+            exchange_user = employee.GetExchangeUser()
+            if exchange_user is not None:
+                manager = exchange_user.GetExchangeUserManager()
+                if manager is not None:
+                    manager_user = manager.GetExchangeUser()
+                    if manager_user is not None and manager_user.EmployeeID == employee_code:
+                        # Add the employee to the direct reports list
+                        direct_reports.append(employee)
+                        # Recursively traverse the direct reports of the employee
+                        traverse_org_hierarchy(exchange_user.EmployeeID, direct_reports)
+        employee = gal.AddressEntries.GetNext()
+
+# Get all the direct reports till the leaf layer for a particular employee code
+employee_code = "12345" # Replace with the employee code you want to query
+direct_reports = []
+traverse_org_hierarchy(employee_code, direct_reports)
+
+# Print the name and email address of each direct report
+for report in direct_reports:
+    print(report.Name)
+    print(report.Address)
