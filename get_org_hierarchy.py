eff86b56bebce05dce27af62e5e4f025ea72820c
jewel-t <130291847+jewel-t@users.noreply.github.com>
2023-04-09 22:17:11 +0800

    Update get_org_hierarchy.py

--- get_org_hierarchy.py
+++ get_org_hierarchy.py
@@ -1,21 +1,45 @@
-import win32com.client
+import csv
 
+def get_subordinates(user, level, writer):
+    """
+    Recursively retrieves the subordinates of a given user and prints the hierarchy tree to console and CSV file.
+    """
+
+    # Check if user exists
+    if user is None:
+        return
+
+    # Get the user's name and title
+    name = user.Name
+    title = user.JobTitle
+
+    # Print the user's details to console
+    print(" " * level + "- " + name + " (" + title + ")")
+
+    # Write the user's details to CSV file
+    writer.writerow([name, title, user.Manager.Name if user.Manager else ""])
+
+    # Get the user's subordinates
+    subordinates = user.GetDirectReports()
+
+    # Recursively call this function on each subordinate
+    for subordinate in subordinates:
+        get_subordinates(subordinate, level + 1, writer)
+
+
+# Connect to Outlook
 outlook = win32com.client.Dispatch("Outlook.Application")
 namespace = outlook.GetNamespace("MAPI")
 
-def get_subordinates(email_address, level=0):
-    recipient = namespace.CreateRecipient(email_address)
-    if recipient.Resolve():
-        exchange_user = recipient.AddressEntry.GetExchangeUser()
-        if exchange_user:
-            print("|   " * level + "+-- " + exchange_user.Name)
-            for subordinate in exchange_user.GetDirectReports():
-                subordinate_email = subordinate.GetExchangeUser().PrimarySmtpAddress
-                get_subordinates(subordinate_email, level=level+1)
-        else:
-            print("|   " * level + "+-- " + email_address + " (no ExchangeUser found)")
-    else:
-        print("|   " * level + "+-- " + email_address + " (unable to resolve)")
-        
-# Replace with the email address of the employee whose subordinates you want to retrieve
-get_subordinates("jsmith@example.com")
+# Get the root user (CEO)
+ceo = namespace.CreateRecipient("CEO's Email Address").AddressEntry.GetExchangeUser()
+
+# Open the CSV file
+with open("hierarchy.csv", "w", newline="") as csvfile:
+    writer = csv.writer(csvfile)
+
+    # Write the header row to CSV file
+    writer.writerow(["Name", "Title", "Manager"])
+
+    # Recursively get the subordinates of the CEO and print the hierarchy tree to console and CSV file
+    get_subordinates(ceo, 0, writer)
