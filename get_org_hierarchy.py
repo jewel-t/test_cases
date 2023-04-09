253e7ca90645ef19b6f69ad71597d794264dba0e
jewel-t <130291847+jewel-t@users.noreply.github.com>
2023-04-09 21:50:26 +0800

    Update get_org_hierarchy.py

--- get_org_hierarchy.py
+++ get_org_hierarchy.py
@@ -2,26 +2,16 @@ import win32com.client
 
 outlook = win32com.client.Dispatch("Outlook.Application")
 namespace = outlook.GetNamespace("MAPI")
-gal = namespace.AddressLists.Item("Global Address List")
-employee_email = "john.smith@example.com"  # Replace with employee email address
-subordinate_dict = {}
 
-visited = set()  # Keep track of visited employees
-
-def find_subordinates(employee_email):
-    try:
-        employee = gal.AddressEntries.GetExchangeUserFromSMTPAddress(employee_email)
-        subordinates = employee.GetDirectReports()
-        subordinate_dict[employee_email] = [subordinate.PrimarySmtpAddress for subordinate in subordinates]
-        visited.add(employee_email)  # Add the current employee to the visited set
-        if subordinates:
-            for subordinate in subordinates:
-                if subordinate.PrimarySmtpAddress not in visited:  # Only visit new employees
-                    find_subordinates(subordinate.PrimarySmtpAddress)
-    except AttributeError:
-        print(f"AttributeError: Email address {employee_email} is not associated with an Exchange user.")
-    except Exception as e:
-        print(f"Error finding subordinates for employee {employee_email}: {e}")
-
-find_subordinates(employee_email)
-print(subordinate_dict)
+email_address = "jsmith@example.com"  # Replace with the email address
+recipient = namespace.CreateRecipient(email_address)
+if recipient.Resolve():
+    exchange_user = recipient.AddressEntry.GetExchangeUser()
+    if exchange_user:
+        print(f"Alias: {exchange_user.Alias}")
+        print(f"Name: {exchange_user.Name}")
+        print(f"Department: {exchange_user.Department}")
+    else:
+        print(f"No ExchangeUser found for {email_address}")
+else:
+    print(f"Unable to resolve {email_address}")
