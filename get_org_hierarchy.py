a74bb631a97435657a71f221bd4392318b102769
jewel-t <130291847+jewel-t@users.noreply.github.com>
2023-04-09 21:09:27 +0800

    Update get_org_hierarchy.py

--- get_org_hierarchy.py
+++ get_org_hierarchy.py
@@ -1,43 +1,20 @@
 import win32com.client
 
-# Connect to Outlook application
-outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
+outlook = win32com.client.Dispatch("Outlook.Application")
+namespace = outlook.GetNamespace("MAPI")
+gal = namespace.AddressLists.Item("Global Address List")
+employee_id = "John.Smith@company.com"  # Update with your own employee ID
+subordinate_dict = {}  # This dictionary will store the subordinate information
 
-# Get the Global Address List
-gal = outlook.Session.GetGlobalAddressList()
+def find_subordinates(employee_id):
+    try:
+        employee = gal.AddressEntries(employee_id).GetExchangeUser()
+        subordinates = employee.GetDirectReports()
+        subordinate_dict[employee_id] = [subordinate.PrimarySmtpAddress for subordinate in subordinates]
+        for subordinate in subordinates:
+            find_subordinates(subordinate.PrimarySmtpAddress)
+    except Exception as e:
+        print(f"Error finding subordinates for employee {employee_id}: {e}")
 
-# Define a recursive function to traverse the organization hierarchy
-def traverse_org_hierarchy(employee_code, direct_reports):
-    print(f"Searching for employee with code {employee_code}")
-    # Get the contact information for the employee
-    employee = gal.AddressEntries.GetFirst()
-    while employee:
-        print(f"Checking employee {employee.Name} with type {employee.Type}")
-        if employee.Type == "EX":
-            exchange_user = employee.GetExchangeUser()
-            if exchange_user is not None:
-                manager = exchange_user.GetExchangeUserManager()
-                if manager is not None:
-                    manager_user = manager.GetExchangeUser()
-                    if manager_user is not None and manager_user.EmployeeID == employee_code:
-                        # Add the employee to the direct reports list
-                        direct_reports.append(employee)
-                        print(f"Found direct report {employee.Name}")
-                        # Recursively traverse the direct reports of the employee
-                        traverse_org_hierarchy(exchange_user.EmployeeID, direct_reports)
-        employee = gal.AddressEntries.GetNext()
-        if employee is None:
-            break
-
-# Get all the direct reports till the leaf layer for a particular employee code
-employee_code = "12345" # Replace with the employee code you want to query
-direct_reports = []
-traverse_org_hierarchy(employee_code, direct_reports)
-
-# Print the name and email address of each direct report
-for report in direct_reports:
-    print(report.Name)
-    print(report.Address)
-    
-# Print the number of direct reports found
-print(f"Total direct reports found: {len(direct_reports)}")
+find_subordinates(employee_id)
+print(subordinate_dict)
