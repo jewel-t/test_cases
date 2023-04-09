7b3b3de121ed8961d830a51fb409b4b909914a2d
jewel-t <130291847+jewel-t@users.noreply.github.com>
2023-04-09 21:19:30 +0800

    Update get_org_hierarchy.py

--- get_org_hierarchy.py
+++ get_org_hierarchy.py
@@ -3,18 +3,19 @@ import win32com.client
 outlook = win32com.client.Dispatch("Outlook.Application")
 namespace = outlook.GetNamespace("MAPI")
 gal = namespace.AddressLists.Item("Global Address List")
-employee_alias = "John.Smith"  # Update with your own employee alias
-subordinate_dict = {}  # This dictionary will store the subordinate information
+employee_email = "john.smith@example.com"  # Replace with employee email address
+subordinate_dict = {}
 
-def find_subordinates(employee_alias):
+def find_subordinates(employee_email, depth=0):
     try:
-        employee = gal.AddressEntries(employee_alias).GetExchangeUser()
+        employee = gal.AddressEntries.GetExchangeUserFromSMTPAddress(employee_email)
         subordinates = employee.GetDirectReports()
-        subordinate_dict[employee_alias] = [subordinate.Alias for subordinate in subordinates]
-        for subordinate in subordinates:
-            find_subordinates(subordinate.Alias)
+        subordinate_dict[employee_email] = [subordinate.PrimarySmtpAddress for subordinate in subordinates]
+        if subordinates:
+            for subordinate in subordinates:
+                find_subordinates(subordinate.PrimarySmtpAddress, depth+1)
     except Exception as e:
-        print(f"Error finding subordinates for employee {employee_alias}: {e}")
+        print(f"Error finding subordinates for employee {employee_email}: {e}")
 
-find_subordinates(employee_alias)
+find_subordinates(employee_email)
 print(subordinate_dict)
